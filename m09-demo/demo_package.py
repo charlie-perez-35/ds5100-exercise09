0c0551004babb2aeac9895dc97ea7f985c9c4ff2
charlie-perez-35 <123101190+charlie-perez-35@users.noreply.github.com>
2024-07-02 11:16:36 -0400

    Add files via upload

---
+++ m09-demo/demo_package.py
@@ -0,0 +1,9 @@
+# I think this is how the module is supposed to work?
+
+def add_numbers(a, b):
+    return a + b
+
+# look man I spent most of this time trying to figure out how to generate my SSH key to use git clone
+# this is as far as I got because I got wayyyy behind and then was completely lost
+# sorry
+# will figure it out with the homework
---
+++ m09-demo/setup.py
@@ -0,0 +1,15 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='1.0.0',
+    url='https://github.com/charlie-perez-35/ds5100-exercise09',
+    author='Charlie Perez',
+    author_email='cwp5xyj@virginia.edu',
+    description='Demonstration',
+    license='MIT'
+    packages=['demo_package']
+)
+    
